6d47302f7b54903b922589b12da31ae903067fba
Dani Latorre <danilat83@gmail.com>
2011-04-05 01:20:45 +0200

    not used

--- grails-app/domain/localizable/Item.groovy
+++
@@ -1,9 +0,0 @@
-package localizable
-
-import com.grails.plugins.localizable.Localizable
-
-class Item implements Localizable {
-
-    static constraints = {
-    }
-}
--- src/groovy/com/grails/plugins/localizable/Localizable.groovy
+++
@@ -1,3 +0,0 @@
-package com.grails.plugins.localizable
-interface Localizable{
-}
